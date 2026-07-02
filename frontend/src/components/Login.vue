2ea23ca299cf963ea8ff978e6b9b2d1ca701f9ba
RBR <31734706+rashidtv@users.noreply.github.com>
2026-07-02 09:49:54 +0000

    fix

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -359,13 +359,6 @@ export default {
   display: block;
 }
 
-/* Desktop only */
-@media (min-width: 1024px) {
-  .login-banner-image {
-    transform: translateX(50px);
-  }
-}
-
 /* ===== FALLBACK (NO BANNER) ===== */
 .banner-fallback {
   text-align: center;
