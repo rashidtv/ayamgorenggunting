332538228cb334fe9bee5a7c3f52aca4cb21def2
Muhammad Rashid Narayanan <rashid828@gmail.com>
2026-07-22 18:00:36 +0800

    fix

--- frontend/src/components/StallAdminPanel.vue
+++ frontend/src/components/StallAdminPanel.vue
@@ -1832,8 +1832,6 @@ export default {
     return Math.min(this.performancePage * this.itemsPerPage, this.filteredPerformanceList.length)
   },
 
-}
-
     totalPages() {
       return Math.ceil(this.filteredInventoryStalls.length / this.itemsPerPage) || 1
     },
@@ -2246,7 +2244,6 @@ stallCurrentPage: {
     this.performanceSortBy = 'rank'
     this.performanceSortOrder = 'asc'
   },
-  }
 
     async updateStock(materialName, newLevel, stallId) {
       try {
